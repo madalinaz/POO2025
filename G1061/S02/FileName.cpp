1611c160edb290e2e1897a54052600d68a3b1943
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-10-12 15:32:16 +0300

    S02

---
+++ G1061/S02/FileName.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+void elim_primul(int*& v, int& n) {
+	//p1 - se calculeaza noua dimensiune
+	int dim = n - 1;
+	//p2 - se aloca zona de memorie ptr noul vector
+	int* aux;
+	aux = new int[dim];
+	//p3 - se copiaza in aux ce este nevoie
+	int k = 0;
+	for (int i = 1; i < n; i++)
+		aux[k++] = v[i];
+	//p4 - dezalocam fostul vector
+	delete[] v;
+	//p5 - vector vechi pointeaza spre vector nou
+	// + actualizare dimensiunea
+	v = aux;
+	n = dim;
+}
+int main()
+{
+	int* v;
+	int n;
+	cin >> n;
+	v = new int[n];  //vectorul cu n int-uri
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	cout << endl << "vector initial: ";
+	for (int i = 0; i < n; i++)
+		cout << v[i] << " ";
+	cout << endl;
+	//exercitiul 1) Functia care elimina 
+	// primul elem dintr-un vector
+	elim_primul(v, n);
+	cout << endl << "vector dupa ex1: ";
+	for (int i = 0; i < n; i++)
+		cout << v[i] << " ";
+	//exercitiul 2) Functie care primeste 
+	// un vector si elimina prima valoare 
+	// para daca exista
+
+
+	delete[] v;
+	return 0;
+}
