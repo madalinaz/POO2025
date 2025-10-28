0542fe237b7cfa6005dab9e079f564a2e3cd8f1e
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-10-28 17:36:35 +0200

    S05

--- G1045/S05/FileName.cpp
+++ G1045/S05/FileName.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+bool apartine(int* v, int n, int x);
+
 class Magazin {
 	string denumire = "Anonim";
 	int nrProd = 0;
@@ -47,12 +49,53 @@ public:
 			cout << this->coduri[i] << " ";
 		}
 	}
+
+	//meth care adauga un nou elem in vectorul de coduri
+	void adaugaCod(int _codNou) {
+		if (!apartine(this->coduri, this->nrProd, _codNou)) {
+			int* copie = new int[this->nrProd + 1];
+			int i;
+			for (i = 0;i<this->nrProd 
+				&& this->coduri[i]<_codNou; i++) {
+				copie[i] = this->coduri[i];
+			}
+			copie[i] = _codNou;
+			for (; i < this->nrProd; i++) {
+				copie[i+1] = this->coduri[i];
+			}
+			delete[] this->coduri;
+			//this->coduri = nullptr;
+			this->coduri = copie;
+			this->nrProd++;
+		}
+	}
 };
+
+//functie globala care verifica daca un elem apartine 
+// unui vector
+//ca sa puteti apela fct globala in clasa, trebuie deasupra clasei
+
+bool apartine(int* v, int n, int x) {
+	for (int i = 0; i < n; i++) {
+		if (v[i] == x) {
+			return true;
+		}
+	}
+	return false;
+}
 
 int main() {
 	Magazin m1;
 	int v[] = { 10,5,7,10,3,4,7,5,10 };
 	Magazin m2("ABC", 9, v);
+	m2.afisare();
+	m2.adaugaCod(8);
+	m2.afisare();
+	m2.adaugaCod(15);
+	m2.afisare();
+	m2.adaugaCod(2);
+	m2.afisare();
+	m2.adaugaCod(10);
 	m2.afisare();
 	return 0;
 }
