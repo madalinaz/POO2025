8792872f436767b94ffeed233c7381612e6bd71b
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-10-28 16:28:43 +0200

    S05

--- G1048/S05/FileName.cpp
+++ G1048/S05/FileName.cpp
@@ -15,27 +15,37 @@ public:
 	Magazin() {
 	}
 
-	Magazin(string _denumire, int _nrProduse, int* _coduriProdus){
+	//Magazin(string _denumire, int _nrProduse, int* _coduriProdus){
+	//	this->denumire = _denumire;
+	//	//validari in cadrul constructorului
+	//	if (_nrProduse > 0 && _coduriProdus != nullptr) {
+	//		int nrDubluri = 0;
+	//		for (int i = 0; i < _nrProduse; i++) {
+	//			for (int j = i + 1; j < _nrProduse; j++) {
+	//				if (_coduriProdus[i] == _coduriProdus[j]
+	//					&& _coduriProdus[i] != -1) {
+	//					nrDubluri++;
+	//					_coduriProdus[j] = -1;
+	//				}
+	//			}
+	//		}
+	//		this->nrProduse = _nrProduse - nrDubluri;
+	//		this->coduriProdus = new int[this->nrProduse];
+	//		int k = 0;
+	//		for (int i = 0; i < _nrProduse; i++) {
+	//			if (_coduriProdus[i] != -1) {
+	//				this->coduriProdus[k++] = _coduriProdus[i];
+	//			}
+	//		}
+	//	}
+	//}
+
+	Magazin(string _denumire, int _nrProduse, int* _coduriProdus) {
 		this->denumire = _denumire;
 		//validari in cadrul constructorului
 		if (_nrProduse > 0 && _coduriProdus != nullptr) {
-			int nrDubluri = 0;
-			for (int i = 0; i < _nrProduse; i++) {
-				for (int j = i + 1; j < _nrProduse; j++) {
-					if (_coduriProdus[i] == _coduriProdus[j]
-						&& _coduriProdus[i] != -1) {
-						nrDubluri++;
-						_coduriProdus[j] = -1;
-					}
-				}
-			}
-			this->nrProduse = _nrProduse - nrDubluri;
-			this->coduriProdus = new int[this->nrProduse];
-			int k = 0;
-			for (int i = 0; i < _nrProduse; i++) {
-				if (_coduriProdus[i] != -1) {
-					this->coduriProdus[k++] = _coduriProdus[i];
-				}
+			for (int i = 0;  i < _nrProduse; i++) {
+				adaugaCod(_coduriProdus[i]);
 			}
 		}
 	}
@@ -59,14 +69,32 @@ public:
 		}
 		if (ok) {
 			//se baga _codNou la sfarsit de vector
-			
+			int* copie = new int[this->nrProduse + 1];
+			for (int i = 0; i < this->nrProduse; i++) {
+				copie[i] = this->coduriProdus[i];
+			}
+			copie[this->nrProduse] = _codNou;
+			delete[] this->coduriProdus;
+			this->coduriProdus = nullptr;
+			this->coduriProdus = copie;
+			this->nrProduse++;
 		}
 	}
 };
 
+//trebuie deasupra clasei daca vrem sa o apelam in clasa
+//fct globala care verifica daca un elem apartine unui vector
+bool apartine(int* v, int n, int x) {
+	//to do home
+}
+
 int main() {
 	int v[] = { 10,5,7,5,10,10,9,8 };
 	Magazin m1("ABC", 8, v);
+	m1.afisare();
+	m1.adaugaCod(10);
+	m1.afisare();
+	m1.adaugaCod(99);
 	m1.afisare();
 	return 0;
 }
