c446a07912382162f32c0ccc207935e2b5b510ad
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-10-30 16:00:40 +0200

    C05

--- Curs_IE/C05/FileName.cpp
+++ Curs_IE/C05/FileName.cpp
@@ -11,7 +11,7 @@ public:
 		cout << "\nApel constructor fara param";
 	}
 
-	Produs(const char* _denumire, int _stoc, double _pret) :Produs() {
+	Produs(const char* _denumire, int _stoc, double _pret){
 		cout << "\nApel constructor cu toti param";
 		if (_denumire != nullptr) {
 			this->denumire = new char[strlen(_denumire) + 1];
@@ -23,6 +23,17 @@ public:
 		if (_pret > 0) {
 			this->pret = _pret;
 		}
+	}
+
+	//to do home->const protejeaza dar nu chiar tot
+	Produs(const Produs& p) {
+		cout << "\nApel constructor copiere";
+		if (p.denumire != nullptr) {
+			this->denumire = new char[strlen(p.denumire) + 1];
+			strcpy(this->denumire, p.denumire);
+		}
+		this->stoc = p.stoc;
+		this->pret = p.pret;
 	}
 
 	void afisare() {
@@ -37,13 +48,57 @@ public:
 		cout << "\nPret: " << this->pret;
 	}
 
+	~Produs() {
+		cout << "\nApel destructor";
+		if (this->denumire != nullptr) {
+			delete[] this->denumire;
+			this->denumire = nullptr;
+		}
+	}
 };
+
+Produs& fct(Produs& p) {
+	//to do...
+	return p;
+}
+
+Produs fct2(Produs p[10], int n) {
+	return p[0];
+}
+
+Produs& fct3(Produs p[10], int n) {
+	return p[0];
+}
+
+Produs& fct4(Produs p) {
+	Produs p2;
+	//.....
+	return p2;
+}
 
 
 int main() {
-	
-	
+	Produs p1;
+	p1.afisare();
+	Produs p2("Carte", 100, 150);
+	p2.afisare();
+	Produs p3(p2);//o copie a unui obiect ->constructorul de copiere
+	p3.afisare();
 
-	
+	//context de apel explicit sau nu destructor
+	Produs* pp = new Produs();
+	delete pp;
+	Produs* pp2 = new Produs[5];
+	delete[] pp2;
+
+	{
+		Produs p4;
+	}
+	cout << "\n----Inchidere }---";
+
+	cout << "\n------------fct--------";
+	fct(p2);
+	cout << "\n------------fct--------";
+
 	return 0;
 }
