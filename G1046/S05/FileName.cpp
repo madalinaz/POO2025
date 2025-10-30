f588cdf3dd5cbfe4a8b09b702dad3632c4dc28f5
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-10-30 17:30:04 +0200

    S05

--- G1046/S05/FileName.cpp
+++ G1046/S05/FileName.cpp
@@ -24,34 +24,43 @@ public:
 	}*/
 
 	//cu validari in interiorul constructorului
+	//Magazin(string _denumire, int _nrProduse, int* _coduri) {
+	//	this->denumire = _denumire;
+	//	if (_nrProduse > 0 && _coduri != nullptr) {
+	//		int ct = 1;
+	//		for (int i = 1; i < _nrProduse; i++) {
+	//			bool gasit = false;
+	//			for (int j = 0; j < i && gasit == false; j++) {
+	//				if (_coduri[i] == _coduri[j])
+	//					gasit = true;
+	//			}
+	//			if (!gasit) {
+	//				ct++;
+	//			}
+	//		}
+	//		this->coduri = new int[ct];
+	//		this->nrProduse = ct;
+	//		int k = 0;
+	//		this->coduri[k++] = _coduri[0];
+	//		for (int i = 1; i < _nrProduse; i++) {
+	//			bool gasit = false;
+	//			for (int j = 0; j < i && gasit == false; j++) {
+	//				if (_coduri[i] == _coduri[j])
+	//					gasit = true;
+	//			}
+	//			if (!gasit) {
+	//				this->coduri[k++] = _coduri[i];
+	//			}
+	//		}
+	//	}
+	//}
+
+	//validari folosind meth de adaugaCod
 	Magazin(string _denumire, int _nrProduse, int* _coduri) {
 		this->denumire = _denumire;
 		if (_nrProduse > 0 && _coduri != nullptr) {
-			int ct = 1;
-			for (int i = 1; i < _nrProduse; i++) {
-				bool gasit = false;
-				for (int j = 0; j < i && gasit == false; j++) {
-					if (_coduri[i] == _coduri[j])
-						gasit = true;
-				}
-				if (!gasit) {
-					ct++;
-				}
-			}
-			this->coduri = new int[ct];
-			this->nrProduse = ct;
-			int k = 0;
-			this->coduri[k++] = _coduri[0];
-			for (int i = 1; i < _nrProduse; i++) {
-				bool gasit = false;
-				for (int j = 0; j < i && gasit == false; j++) {
-					if (_coduri[i] == _coduri[j])
-						gasit = true;
-				}
-				if (!gasit) {
-					this->coduri[k++] = _coduri[i];
-				}
-			}
+			for (int i = 0; i < this->nrProduse; i++)
+				adaugaCod(_coduri[i]);
 		}
 	}
 
@@ -64,11 +73,28 @@ public:
 	}
 
 	//metoda care adauga un nou cod in vectorul coduri
+	void adaugaCod(int _cod) {
+		for (int i = 0; i < this->nrProduse; i++) {
+			if (this->coduri[i] == _cod)
+				return;
+		}
+		int* aux = new int[this->nrProduse + 1];
+		for (int i = 0; i < this->nrProduse; i++)
+			aux[i] = this->coduri[i];
+		aux[this->nrProduse] = _cod;
+		delete[] this->coduri;
+		this->coduri = aux;
+		this->nrProduse++;
+	}
 };
 
 int main() {
 	int v[]{ 10,45,13,40,13, 12 };
 	Magazin m1("ABC", 5, v);
+	m1.afisare();
+	m1.adaugaCod(14);
+	m1.afisare();
+	m1.adaugaCod(13);
 	m1.afisare();
 	return 0;
 }
