c5ee9d6e5d7af916fa5baec8df1b16016f3600c9
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-10-30 16:54:01 +0200

    S05

---
+++ G1046/S05/FileName.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Magazin {
+	string denumire = "Anonim";
+	int nrProduse = 0;
+	int* coduri = nullptr;//retine codurile produselor ( se respecta unicitatea)
+
+public:
+	//constructor fara param
+	Magazin(){ }
+	//constructor cu denumire
+	
+	//constructor care primeste toti param
+	//fara validari de continut vector
+	Magazin(string _denumire, int _nrProduse, int* _coduri) {
+		this->denumire = _denumire;
+		if (_nrProduse > 0 && _coduri != nullptr) {
+			this->nrProduse = _nrProduse;
+			this->coduri = new int[this->nrProduse];
+			for (int i = 0; i < this->nrProduse; i++)
+				this->coduri[i] = _coduri[i];
+		}
+	}
+
+	void afisare() {
+		cout << "\nDenumire: " << this->denumire;
+		cout << "\nNr produse: " << this->nrProduse;
+		cout << "\nCoduri: ";
+		for (int i = 0; i < this->nrProduse; i++)
+			cout << this->coduri[i] << " ";
+	}
+
+	//metoda care adauga un nou cod in vectorul coduri
+};
+
+int main() {
+	int v[]{ 10,45,13,40,13, 12 };
+	Magazin m1("ABC", 5, v);
+	m1.afisare();
+	return 0;
+}
