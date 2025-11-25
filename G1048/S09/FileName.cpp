e053d035103c7a6f2985911da89191794ab1c1d0
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-11-25 16:21:07 +0200

    S09

--- G1048/S09/FileName.cpp
+++ G1048/S09/FileName.cpp
@@ -46,6 +46,7 @@ public:
 					this->putereArme[i] = c.putereArme[i];
 			}
 		}
+		return *this;
 	}
 
 	~Caracter() {
@@ -75,15 +76,45 @@ public:
 
 	//TEMA 2. DE REGESTIONAT ACEASTA IMPLEMENTARE ASTFEL INCAT SA NU NE FOLOSIM DE CELALALT OPERATOR +=
 	//actualizeaza this-ul prin preluarea din c a tuturor armelor
-	Caracter& operator+=(Caracter c) {
-		for (int i = 0; i < c.nrArme; i++) {
-			//this->operator+=(c.putereArme[i]);
-			(*this) += c.putereArme[i];
+	Caracter& operator+=(Caracter& c) {
+		if (this != &c) {
+			for (int i = 0; i < c.nrArme; i++) {
+				//this->operator+=(c.putereArme[i]);
+				(*this) += c.putereArme[i];
+			}
+			if (c.putereArme != nullptr) {
+				delete[] c.putereArme;
+				c.putereArme = nullptr;
+				c.nrArme = 0;
+			}
 		}
 		return *this;
 	}
 
+	//de adaugat validare pe nivel viata ca sa nu depaseasca 100
+	//pre-fixata
+	Caracter& operator++() {
+		this->nivelViata *= 1.1;
+		return *this;//returnam starea de dupa modificare
+	}
+
+	//de adaugat validare pe nivel viata ca sa nu depaseasca 100
+	//post-fixata
+	Caracter operator++(int) {
+		Caracter copie = *this;
+		this->nivelViata *= 1.1;
+		return copie;
+	}
+
+	double operator[](int _index) {
+		if (_index >= 0 && _index < this->nrArme) {
+			return this->putereArme[_index];
+		}
+	}
+
 	friend ostream& operator<<(ostream& out, const Caracter& c);
+
+	friend Caracter operator*(int _x, const Caracter& c);
 };
 
 int Caracter::minViata = 10;
@@ -99,14 +130,43 @@ ostream& operator<<(ostream& out, const Caracter& c) {
 	return out;
 }
 
+Caracter operator*(int _x, const Caracter& c) {
+	if (_x > 0) {
+		Caracter rez = c;
+		rez.nivelViata *= _x;
+		if (rez.nivelViata >= 100)
+			rez.nivelViata = 100;
+		return rez;
+	}
+}
+
+//TEMA3
+class Joc {
+	//colectie de Caractere (NU putem stoca obiecte, merg doar pointeri)
+	//+= un nou caracter in joc
+	//cum gestionati prin operatori situatia in care un caracter moare
+};
+
+//TEMA4 
+//Cum gestionati astfel incat clasa Joc sa permita crearea unui UNIC obiect de tip Joc (Singleton)
+
 int main() {
 	Caracter c1(12, "Zburator 1");
 	cout << c1;
 	c1 += 10;//adaug o arma cu putere de lovire 10
 	c1 += c1 += 12;
 	cout << c1;
-	//c1++;//cresc nivelul de viata cu 10% fata de nivelul anterior
+	c1++;//cresc nivelul de viata cu 10% fata de nivelul anterior
+	c1.operator++(2);
+	++c1;
+	c1.~Caracter();
+	cout << c1;
 	Caracter c2(13);
-	//c2 = 2 * c1;//returneaza un obj care are viata dublata in limita intervalului
+	c2 = 2 * c1;//returneaza un obj care are viata dublata in limita intervalului
+	operator*(2, c1);
+
+	//operator [] pentru pastrarea incapsularii
+	double putere = c1[2];//preluarea nivelul de putere a armei de pe index 2
+	//c1[2] = -100;
 	return 0;
 }
