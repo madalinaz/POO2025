4bf0cb098443790fcf2dd5b174dc9d49b3c9bd9b
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-11-25 16:33:07 +0200

    S09

---
+++ G1045/S09/FileName.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+
+//TEMA1 ->clasa Caracter in care putereArme este alocat static
+class Caracter {
+	const int id;
+	string denumire = "-";
+	int nivelViata = 100;//apartine intervalului (0;100]
+	int nrArme = 0;
+	double* putereArme = nullptr;
+	static int minViata;
+
+public:
+	Caracter(int _id) :id(_id) {
+	}
+
+	Caracter(int _id, string _denumire) :id(_id) {
+		this->denumire = _denumire;
+	}
+
+	Caracter(const Caracter& c) :id(c.id) {
+		this->denumire = c.denumire;
+		this->nivelViata = c.nivelViata;
+		if (c.nrArme > 0 && c.putereArme != nullptr) {
+			this->nrArme = c.nrArme;
+			this->putereArme = new double[this->nrArme];
+			for (int i = 0; i < this->nrArme; i++)
+				this->putereArme[i] = c.putereArme[i];
+		}
+	}
+
+	Caracter& operator=(const Caracter& c) {
+		if (this != &c) {
+			if (this->putereArme != nullptr) {
+				delete[] this->putereArme;
+				this->putereArme = nullptr;
+				this->nrArme = 0;
+			}
+			this->denumire = c.denumire;
+			this->nivelViata = c.nivelViata;
+			if (c.nrArme > 0 && c.putereArme != nullptr) {
+				this->nrArme = c.nrArme;
+				this->putereArme = new double[this->nrArme];
+				for (int i = 0; i < this->nrArme; i++)
+					this->putereArme[i] = c.putereArme[i];
+			}
+		}
+		return *this;
+	}
+
+	~Caracter() {
+		if (this->putereArme != nullptr) {
+			delete[] this->putereArme;
+			this->putereArme = nullptr;
+			this->nrArme = 0;
+		}
+	}
+
+};
+
+int Caracter::minViata = 10;
+
+int main() {
+
+	return 0;
+}
