ef6e0fe0a3e29518d2ffd93f70f249050b53e8ea
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-12-03 08:53:10 +0200

    S10

--- G1045/S10/FileName.cpp
+++ G1045/S10/FileName.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+//afisare prin ostream de enum
+//citire prin istream de enum
 
 class Angajat {
 
@@ -47,7 +49,17 @@ public:
 	~Angajat() {
 		cout << "\nApel destructor Angajat";
 	}
+
+	friend istream& operator>>(istream& in, Angajat& a);
 };
+
+istream& operator>>(istream& in, Angajat& a) {
+	cout << "\nIntroduceti nume: ";
+	in >> a.nume;
+	cout << "Introduceti salariu baza: ";
+	in >> a.salariuBaza;
+	return in;
+}
 
 ostream& operator<<(ostream& out, const Angajat& a) {
 	out << "\nNume: " << a.nume;
@@ -55,7 +67,87 @@ ostream& operator<<(ostream& out, const Angajat& a) {
 	return out;
 }
 
-int main() {
+//am creat o arborescenta -> DP Composite
+//Manager is a Angajat
+//Manager has a Angajat (home)
+class Manager: public Angajat {
+	int nrSubordonati = 0;
+	static float bonusPerSubordonat;
 
+public:
+	Manager() {
+		cout << "\nApel const fara param Manager";
+	}
+
+	Manager(string _nume, float _salariuBaza, int _nrSubordonati):Angajat(_nume,_salariuBaza) {
+		cout << "\nApel const cu param Manager";
+		this->nrSubordonati = _nrSubordonati;
+	}
+
+	Manager(Angajat a, int _nrSubordonati):Angajat(a) {
+		cout << "\nApel const cu param 2 Manager";
+		this->nrSubordonati = _nrSubordonati;
+	}
+
+	//upcast de la copil la parinte, de la derivat la baza, de la Manager la Angajat
+	Manager(const Manager& m):Angajat(m) {
+		cout << "\nApel const copiere Manager";
+		this->nrSubordonati = m.nrSubordonati;
+	}
+
+	Manager& operator=(const Manager& m) {
+		cout << "\nApel op= Manager";
+		if (this != &m) {
+			this->Angajat::operator=(m);//al doilea upcast
+			this->nrSubordonati = m.nrSubordonati;
+		}
+		return *this;
+	}
+
+	float calculSalariu() {
+		float salariuAngajat = Angajat::calculSalariu();
+		return salariuAngajat + Manager::bonusPerSubordonat * this->nrSubordonati;
+	}
+
+	~Manager() {
+		cout << "\nApel destructor Manager";
+	}
+
+	friend ostream& operator<<(ostream& out, const Manager& m);
+
+	friend istream& operator>>(istream& in, Manager& m);
+};
+
+float Manager::bonusPerSubordonat = 1000;
+
+ostream& operator<<(ostream& out, const Manager& m) {
+	out << (Angajat&)m;//primul upcast explicit
+	out << "\nNr subordonati: " << m.nrSubordonati;
+	return out;
+}
+
+istream& operator>>(istream& in, Manager& m) {
+	in >> (Angajat&)m;
+	cout << "Introduceti nr subordonati: ";
+	in >> m.nrSubordonati;
+	return in;
+}
+
+int main() {
+	Manager m1;
+	Manager m2("Gigel", 2000, 3);
+	Manager m3(Angajat("Costel", 1000), 3);
+	Manager m4(m2);
+	m1 = m2;
+	cout << "\n-----------------";
+	cout << m1;
+	cout << "\n-----------------";
+
+	Angajat a;
+	//cin >> a;
+	cout << a;
+	cin >> m1;
+	cout << m1;
+	cout << "\nSalariu lui m1 manager: " << m1.calculSalariu();
 	return 0;
 }
