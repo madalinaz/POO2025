5681e67252919c3488bed4cceff6dadd2c284763
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-12-11 14:36:12 +0200

    s11

--- G1047/S11/FileName.cpp
+++ G1047/S11/FileName.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 
-class Angajat {
+//DACA CLASA ANGAJAT ARE O METH VIRTUALA PURA, CUM PUTEM SA INTEGRAM OSTREAM SI SA PUTEM AFISA INTR-O MANIERA PARTEA COMUNA A OBIECTELOR DERIVATE DIN CLASA ANGAJAT
+//HINT DIN JAVA: TOSTRING()
+
+class Om {
+public:
+	void afisare() {
+		cout << "\nAcesta este un om!";
+	}
+};
+
+class Angajat:public Om {
 
 protected:
 	string nume = "Anonim";
@@ -35,9 +45,12 @@ public:
 
 	friend ostream& operator<<(ostream& out, const Angajat& a);
 
-	float calculSalariu() {
+	virtual float calculSalariu() {
+		//pp ca exista o tehnica complexa de calcul salariu
 		return this->salariuBaza;
 	}
+
+	//virtual int getNrZileConcediu() = 0;//meth virtuala pura
 
 	~Angajat() {
 	}
@@ -92,6 +105,14 @@ public:
 	}
 
 	friend ostream& operator<<(ostream& out, const Paznic& m);
+
+	float calculSalariu() override{
+		return Angajat::calculSalariu() + this->nrNoptiLucrate * (1 + Paznic::sporNoapte) * this->salariuBaza / 20;
+	}
+
+	void altaMetodaPaznic() {
+
+	}
 };
 
 float Paznic::sporNoapte = 0.2;
@@ -109,6 +130,32 @@ ostream& operator<<(ostream& out, const Manager& m) {
 }
 
 int main() {
+	Angajat a1("Angajat Gigel", 1000);
+	Manager m1("Manager Gigel", 1000, 10);
+	Paznic p1("Paznic Gigel", 1000, 4);
+	cout << "\n------CALCUL SALARII PE OBIECTE-----";
+	cout << "\nSalariu angajat: " << a1.calculSalariu();
+	cout << "\nSalariu manager: " << m1.calculSalariu();
+	cout << "\nSalariu paznic: " << p1.calculSalariu();
+
+	//fondul de salarii pentru toti angajatii
+	Angajat v[] = { a1,m1,p1 };
+	float totalSalarii = 0;
+	for (int i = 0; i < 3; i++) {
+		totalSalarii += v[i].calculSalariu();
+	}
+	cout << "\nTotal salarii folosind vector obiecte: " << totalSalarii;
+
+	Angajat* vp[] = { &a1,&m1,&p1 };
+	float totalSalarii2 = 0;
+	for (int i = 0; i < 3; i++) {
+		totalSalarii2 += vp[i]->calculSalariu();
+	}
+	cout << "\nTotal salarii folosind vector pointeri: " << totalSalarii2;
+
+	Om vo[] = {Om(), Om()};
+	vo[0].afisare();
+
 
 	return 0;
 }
