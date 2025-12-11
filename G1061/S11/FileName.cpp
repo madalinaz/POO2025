f5e1d6fd5b615682d526edd44b93c4b99cd35f99
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-12-11 10:41:47 +0200

    S11

--- G1061/S11/FileName.cpp
+++ G1061/S11/FileName.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 using namespace std;
 
-//afisare+citire obj de tip enum folosind ostream si istream(HOME)
-
 class Angajat {
 
 protected:
@@ -51,17 +49,7 @@ ostream& operator<<(ostream& out, const Angajat& a) {
 	return out;
 }
 
-//Liskov substitution
-//relatia de tipul is-a
-//mostenire/derivare
-//clasa Angajat ->clasa parinte/baza
-//clasa Manager ->clasa derivata/copil
-
-//Manager is a Angajat (done)
-//Manager has a Angajat (1-M)(to do)
-//Design pattern Composite (modeleaza o arborescenta)
 class Manager :public Angajat {
-	//campuri specifice unui Manager
 	int nrSubordonati = 0;
 
 public:
@@ -100,11 +88,53 @@ ostream& operator<<(ostream& out, const Manager& m) {
 	return out;
 }
 
-//home: istream vreau pentru Angajat si Manager
+class Paznic :public Angajat {
+	int nrZileNoapte = 0;
+	static float sporNoapte;
+
+public:
+	Paznic() {
+	}
+
+	Paznic(string _nume, float _salariuBaza, int _nrZileNoapte) :Angajat(_nume, _salariuBaza) {
+		this->nrZileNoapte = _nrZileNoapte;
+	}
+
+	Paznic(const Paznic& p) :Angajat(p) {
+		this->nrZileNoapte = p.nrZileNoapte;
+	}
+
+	Paznic& operator=(const Paznic& p) {
+		if (this != &p) {
+			this->Angajat::operator=(p);//upcast
+			this->nrZileNoapte = p.nrZileNoapte;
+		}
+		return *this;
+	}
+
+	~Paznic() {
+	}
+
+	friend ostream& operator<<(ostream& out, const Paznic& p);
+};
+
+ostream& operator<<(ostream& out, const Paznic& p) {
+	out << (Angajat)p;
+	out << "\nNr zile noapte: " << p.nrZileNoapte;
+	return out;
+}
+
+float Paznic::sporNoapte = 0.2;
 
 int main() {
 	Angajat a1("Gigel", 1200);
 	Angajat a2("Costel", 1500);
+
+	//depend upon the abstract, not upon the concret
+	//meth virtuale
+	//meth virtuale pure
+	//clase abstracte; interfete
+
 	
 	return 0;
 }
