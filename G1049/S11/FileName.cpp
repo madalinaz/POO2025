025ed7c3ee7086f99a13a9a93a06b31e7f634182
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-12-11 19:07:14 +0200

    S11

---
+++ G1049/S11/FileName.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Angajat{
+
+protected:
+	string nume = "Anonim";
+	float salariuBaza = 0;
+
+public:
+	Angajat() {
+	}
+
+	Angajat(string _nume) {
+		this->nume = _nume;
+	}
+
+	Angajat(string _nume, float _salariuBaza) {
+		this->nume = _nume;
+		this->salariuBaza = _salariuBaza;
+	}
+
+	Angajat(const Angajat& a) {
+		this->nume = a.nume;
+		this->salariuBaza = a.salariuBaza;
+	}
+
+	Angajat& operator=(const Angajat& a) {
+		if (this != &a) {
+			this->nume = a.nume;
+			this->salariuBaza = a.salariuBaza;
+		}
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream& out, const Angajat& a);
+
+	float calculSalariu() {
+		//pp ca exista o regula complexa de calcul salariu
+		return this->salariuBaza;
+	}
+
+	~Angajat() {
+	}
+};
+
+ostream& operator<<(ostream& out, const Angajat& a) {
+	out << "\nNume: " << a.nume;
+	out << "\nSalariu baza: " << a.salariuBaza;
+	return out;
+}
+
+int main() {
+	return 0;
+}
+
