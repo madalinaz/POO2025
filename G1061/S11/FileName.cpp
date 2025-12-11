841c0db71216c1fc13857f131946109763bcd2c8
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-12-11 10:29:30 +0200

    S11

--- G1061/S11/FileName.cpp
+++ G1061/S11/FileName.cpp
@@ -11,27 +11,23 @@ protected:
 
 public:
 	Angajat() {
-		cout << "\nApel constructor fara param Angajat";
 	}
 
-	explicit Angajat(string _nume) {
+	Angajat(string _nume) {
 		this->nume = _nume;
 	}
 
 	Angajat(string _nume, float _salariuBaza) {
-		cout << "\nApel constructor cu param Angajat";
 		this->nume = _nume;
 		this->salariuBaza = _salariuBaza;
 	}
 
 	Angajat(const Angajat& a) {
-		cout << "\nApel constructor copiere Angajat";
 		this->nume = a.nume;
 		this->salariuBaza = a.salariuBaza;
 	}
 
 	Angajat& operator=(const Angajat& a) {
-		cout << "\nApel operator= Angajat";
 		if (this != &a) {
 			this->nume = a.nume;
 			this->salariuBaza = a.salariuBaza;
@@ -45,26 +41,7 @@ public:
 		return this->salariuBaza;
 	}
 
-	bool operator==(const Angajat& a)const {
-		return this->nume == a.nume;
-	}
-
-	bool operator!() {
-		return this->salariuBaza == 0;
-	}
-
-	//cast la string
-	operator string () {
-		return this->nume;
-	}
-
-	//cast la double
-	explicit operator double() {
-		return this->salariuBaza;
-	}
-
 	~Angajat() {
-		cout << "\nApel destructor Angajat";
 	}
 };
 
@@ -89,26 +66,21 @@ class Manager :public Angajat {
 
 public:
 	Manager() {
-		cout << "\nApel constructor fara param Manager";
 	}
 
 	Manager(string _nume, float _salariuBaza, int _nrSubordonati) :Angajat(_nume, _salariuBaza) {
-		cout << "\nApel constructor cu param Manager";
 		this->nrSubordonati = _nrSubordonati;
 	}
 
 	Manager(Angajat a, int _nrSubordonati) :Angajat(a) {
-		cout << "\nApel constructor cu param 2 Manager";
 		this->nrSubordonati = _nrSubordonati;
 	}
 
-	Manager(const Manager& m) :Angajat(m) {//constructor de copiere, folosind upcast
-		cout << "\nApel constructor copiere Manager";
+	Manager(const Manager& m) :Angajat(m) {
 		this->nrSubordonati = m.nrSubordonati;
 	}
 
 	Manager& operator=(const Manager& m) {
-		cout << "\nApel operator= Manager";
 		if (this != &m) {
 			this->Angajat::operator=(m);//upcast
 			this->nrSubordonati = m.nrSubordonati;
@@ -117,20 +89,18 @@ public:
 	}
 
 	~Manager() {
-		cout << "\nApel destructor Manager";
 	}
 
 	friend ostream& operator<<(ostream& out, const Manager& m);
 };
 
 ostream& operator<<(ostream& out, const Manager& m) {
-	out << (Angajat)m;//upcast explicit prin valoare returneaza o copie care ulterior este distrusa
+	out << (Angajat)m;
 	out << "\nNr subordonati: " << m.nrSubordonati;
 	return out;
 }
 
 //home: istream vreau pentru Angajat si Manager
-
 
 int main() {
 	Angajat a1("Gigel", 1200);
