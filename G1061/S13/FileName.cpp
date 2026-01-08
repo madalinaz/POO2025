262b425db0384582672fbfefc636d4b998e218d4
Madalina Zurini <madalina.zurini@csie.ase.ro>
2026-01-08 12:01:59 +0200

    S13

--- G1061/S13/FileName.cpp
+++ G1061/S13/FileName.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 
 //o clasa la alegere care sa contina cel putin un camp numeric (constructor cu param, ostream)
@@ -6,6 +7,51 @@ using namespace std;
 //un fisier text
 //generare raport text pe baza unei colectii de obiecte de tipul clasei care sa contina top n obiecte in functie de campul numeric ales
 
+class Student {
+	string nume="Anonim";
+
+public:
+	friend ostream& operator<<(ostream& out, const Student& s);
+};
+
+ostream& operator<<(ostream& out, const Student& s) {
+	out << s.nume << endl;
+	return out;
+}
+
+//Grupa has a Student
+class Grupa {
+	Student v[10];//alocare statica
+	int nr;
+
+	//Student* vDinamic;
+	//int nrDinamic;
+
+	////daca dorim retinerea prin actualizari a obiectelor, atunci folosim pointeri
+	//Student* v2[10];
+	//int nr2;
+
+	//Student** v3;//dinamic stochez pointeri la student
+	//int nr3;
+public:
+	void generareFisierText(string numeFisier, int ct) {
+		ofstream fOut(numeFisier);
+		//sortare pe vector de Student
+		for (int i = 0; i < ct; i++)
+			fOut << this->v[i];
+		fOut.close();
+	}
+};
+
 int main() {
+	//fisiere text
+	Student s;
+	cout << s;
+	ofstream g("fisier.txt");
+	g << s;//ofstream is a ostream
+	g.close();
+
+	Grupa gr;
+	//gr.generareFisierText("top.txt", 3);
 	return 0;
 }
