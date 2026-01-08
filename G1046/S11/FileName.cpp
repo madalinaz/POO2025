8784f587ef2dde8889535949463059bb2f3a7b42
Madalina Zurini <madalina.zurini@csie.ase.ro>
2026-01-08 16:49:31 +0200

    S12

---
+++ G1046/S11/FileName.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+class Angajat {
+protected:
+	string nume = "Anonim";
+	float salariuBaza = 0;
+
+public:
+	Angajat() {
+
+	}
+
+	Angajat(string _nume) {
+		this->nume = _nume;
+	}
+
+	Angajat(string _nume, float _salariuBaza) {
+		this->nume = _nume;
+		this->salariuBaza = _salariuBaza;
+	}
+
+	Angajat(const Angajat& a) {
+		this->nume = a.nume;
+		this->salariuBaza = a.salariuBaza;
+	}
+
+	Angajat& operator=(const Angajat& a) {
+		if (this != &a) {
+			this->nume = a.nume;
+			this->salariuBaza = a.salariuBaza;
+		}
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream& out, const Angajat& a);
+
+	float calculSalariu() {
+		//pp ca exista o regula complexa de calcul salariu
+		return this->salariuBaza;
+	}
+
+	~Angajat() {
+	}
+};
+
+ostream& operator<<(ostream& out, const Angajat& a) {
+	out << "\nNume: " << a.nume;
+	out << "\nSalariu baza: " << a.salariuBaza;
+	return out;
+}
+
+class Manager :public Angajat {
+	int nrSubordonati = 0;
+
+public:
+	Manager(string _nume, float _salariuBaza, int _nrSubordonati) :Angajat(_nume, _salariuBaza) {
+		this->nrSubordonati = _nrSubordonati;
+	}
+
+
+	friend ostream& operator<<(ostream& out, const Manager& m);
+};
+
+ostream& operator<<(ostream& out, const Manager& m) {
+	out << (Angajat)m;//upcast prin valoare explicit
+	out << "\nNr subordonati: " << m.nrSubordonati;
+	return out;
+}
+
+class Lucrator :public Angajat {
+	int nrOreSuplimentare = 0;
+
+public:
+	Lucrator(string _nume, float _salariuBaza, int _nrOreSuplimentare) :Angajat(_nume, _salariuBaza) {
+		this->nrOreSuplimentare = _nrOreSuplimentare;
+	}
+
+	friend ostream& operator<<(ostream& out, const Lucrator& m);
+};
+
+ostream& operator<<(ostream& out, const Lucrator& m) {
+	out << (Angajat)m;//upcast prin valoare explicit
+	out << "\nNr ore suplimentare: " << m.nrOreSuplimentare;
+	return out;
+}
+
+int main() {
+	Angajat a1("Angajat Gigel", 1000);
+	Manager m1("Manager Costel", 1000, 10);
+	Lucrator l1("Lucrator Marcel", 1000, 25);
+	return 0;
+}
