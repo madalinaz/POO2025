b3f2d94580c77da72426e721e41dd3e886fe7abf
Madalina Zurini <madalina.zurini@csie.ase.ro>
2026-01-09 18:15:28 +0200

    S13

--- G1049/S13/FileName.cpp
+++ G1049/S13/FileName.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
 
+//functie + clasa template/sablon
+//reutilizare de cod/implementa o "interfata"/layer intermediar de comunicare cu cod deja existent
+
+//o clasa care sa faciliteze lucrul cu vectori de obiecte/int
+//partea de populare cu noi valori, sortare, adunare/agregare, ...
+class Colectie {
+	int v[100];
+	int nr = 0;
+
+public:
+	//NU implementam constructori
+
+	void populare(int x) {
+
+	}
+
+
+};
+
 class Angajat {
 	string nume = "";
 	float salariu = 0;
@@ -16,7 +35,6 @@ public:
 		return out;
 	}
 };
-
 
 int main() {
 
