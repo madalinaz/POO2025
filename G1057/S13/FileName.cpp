772c98c2c5b81dce7e57919c4117fc9d92301643
Madalina Zurini <madalina.zurini@csie.ase.ro>
2026-01-09 09:16:12 +0200

    S13

--- G1057/S13/FileName.cpp
+++ G1057/S13/FileName.cpp
@@ -6,20 +6,19 @@ class Angajat {
 	float salariu = 0;
 
 public:
-	Angajat() {
-
-	}
 
 	Angajat(string _nume, float _salariu) {
 		this->nume = _nume;
 		this->salariu = _salariu;
 	}
 
-	friend ostream& operator<<(ostream& out, const Angajat& a) {
-		out << "\nNume: " << a.nume << ", salariu: " << a.salariu;
-		return out;
-	}
+	friend ostream& operator<<(ostream& out, const Angajat& a); 
 };
+
+ostream& operator<<(ostream& out, const Angajat& a){
+	out << "\nNume: " << a.nume << ", salariu: " << a.salariu;
+	return out;
+}
 
 
 int main() {
