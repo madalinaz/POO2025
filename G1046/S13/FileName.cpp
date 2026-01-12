d2729725cd130093dba575bee06d50ca63e5b736
Madalina Zurini <madalina.zurini@csie.ase.ro>
2026-01-12 18:35:15 +0200

    S13

--- G1046/S13/FileName.cpp
+++ G1046/S13/FileName.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class Angajat {
@@ -23,12 +24,45 @@ public:
 //colectie/relatie has a; clasa template
 //o clasa care sa retina informatii pentru fiecare Angajat, o colectie de elemente generice (acea colectie/vector/lista -> un set de sporuri/ un set de concedii/.....)
 
+//clasa template
 //ColectieAngajati has a Angajat
 class ColectieAngajati {
-
+	//vector de pointeri la angajati
+	//matrice de obj de tip generic T
+	//
 };
 
 int main() {
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(15);
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
 
+	vector<int>::iterator it;
+	cout << endl;
+	for (it = v.begin(); it != v.end(); it++) {
+		cout << *it << " ";
+	}
+
+	vector<vector<int>> m; //matricea bi-dimensionala
+	//adaugam pe prima linie din vector valoarea 10;
+	m.push_back(vector<int>{});
+	m[0].push_back(10);
+	m.push_back(vector<int>{});
+	m[1].push_back(20);
+	m[1].push_back(100);
+	m[1].push_back(30);
+	m.push_back(vector<int>{});
+	m[2].push_back(30);
+	m[2].push_back(30);
+	m.push_back(vector<int>{});
+	m[3].push_back(20);
+	m[3].push_back(20);
+	m[3].push_back(20);
+
+	//o metoda/forma de afisare a matricei noastre
 	return 0;
 }
